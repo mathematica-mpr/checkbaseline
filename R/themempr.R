78e088bb17e783c1a9c6709c33bc2b24449604f5
Ignacio Martinez <IMartinez@mathematica-mpr.com>
2016-03-28 10:59:58 -0400

    changes template background to white

--- R/themempr.R
+++ R/themempr.R
@@ -1,7 +1,17 @@
-theme_mpr <- function(base_size = 14,base_family = "sans") {
+#' @title Theme MPR
+#' @param base_size Fonts base size
+#' @param base_family Font family
+#' @return ggplot theme
+#' @examples
+#' theme_mpr(base_size = 14, base_family = "Courier bold")
+#' @export
+
+theme_mpr <- function(base_size = 14, base_family = "sans") {
   # Colors
-  background <- "#F0F0F0"
+  background <- "white" # "#F0F0F0"
   grid <- "#D9D9D9"
+  grid.major <- "grey90"
+  grid.minor <- "grey98"
   axis.text <- "#737373"
   axis.title <- "#525252"
   plot.title <- "#000000"
@@ -10,14 +20,23 @@ theme_mpr <- function(base_size = 14,base_family = "sans") {
   theme_bw(base_size = base_size, base_family = base_family) +
 
     # Set the entire chart region to a light gray color
-    theme(panel.background = element_rect(fill = background, color = background),
-          plot.background = element_rect(fill = background, color = background),
-          panel.border = element_rect(color = background)) +
+    theme(
+      panel.background = element_rect(fill = background, color = background),
+      plot.background = element_rect(fill = background, color = background),
+      panel.border = element_rect(color = background),
+      strip.background = element_rect(fill=background, color = background)
+    ) +
 
     # Grid
-    theme(panel.grid.major = element_line(color = grid, size = .25),
-          panel.grid.minor = element_line(color = grid, size = .15, linetype = "dashed"),
-          axis.ticks = element_blank()) +
+    theme(
+      panel.grid.major = element_line(color = grid.major, size = .25),
+      panel.grid.minor = element_line(
+        color = grid.minor,
+        size = .15,
+        linetype = "dashed"
+      ),
+      axis.ticks = element_blank()
+    ) +
 
     # Legend
     theme(
@@ -26,11 +45,16 @@ theme_mpr <- function(base_size = 14,base_family = "sans") {
       legend.direction = "horizontal",
       legend.box = "vertical",
       legend.key = element_rect(color = background, fill = background)
-      ) +
+    ) +
 
     # Title, and axis labels
     theme(
-      plot.title = element_text(color = plot.title, hjust = 0, size = rel(1.5), face = "bold"),
+      plot.title = element_text(
+        color = plot.title,
+        hjust = 0,
+        size = rel(1.5),
+        face = "bold"
+      ),
       axis.text.x = element_text(color = axis.text),
       axis.text.y = element_text(color = axis.text),
       axis.title.x = element_text(color = axis.title, vjust = 0),
@@ -40,3 +64,4 @@ theme_mpr <- function(base_size = 14,base_family = "sans") {
     # Margins
     theme(plot.margin = unit(c(1, 1, 1, 1), "lines"))
 }
+
